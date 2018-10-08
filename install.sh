a6648bf642e0e9383dd40c7c1eab0b754736d96e
KK <kk@retailnext.net>
2018-10-07 21:41:05 -0700

    Adding Properties file and updated install script

--- install.sh
+++ install.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /usr/bin/psql ];  then 
-    echo "Postgres is not installed, Installing..."
-    yum -y install postgresql92-server postgresql92-devel gcc
-    /sbin/service postgresql92 initdb
-    /sbin/service postgresql92 restart
-    su postgres -c "/usr/bin/psql -U postgres -c \"alter user postgres password 'postgres'\"" >/dev/null
-fi
-
-cat > /var/lib/pgsql92/data/pg_hba.conf << EOF
-# TYPE   DATABASE    USER    ADDRESS    METHOD    OPTIONS
-local    all         all                password
-host     all         all     all        password
-EOF
-
-if [ -f ~/.pgpass ]; then
-    grep "xovis" ~/.pgpass > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "localhost:*:*:xovis:xovis" >> ~/.pgpass
-    fi
-else
-    echo "localhost:*:*:postgres:postgres" >> ~/.pgpass
-    echo "localhost:*:*:xovis:xovis" >> ~/.pgpass
-    chmod 600 ~/.pgpass
-fi
-
-/sbin/service postgresql92 restart
-
 # Create Xovis config database
 /usr/bin/psql -Upostgres -f CreateDatabase.sql
 
@@ -39,7 +12,7 @@ pip install --upgrade pip
 # Create /var/www/status directory
 mkdir -p /var/www/status
 
-# Copy all the resources to /var/www/status 
+# Copy all the resources to /var/www/status
 cp -r resources /var/www/status/
 
 # Change File permissions for all the files under /var/www/status
@@ -62,8 +35,11 @@ rm -rf mycron
 # Copy the Apache configuration file to /etc/httpd/conf.d/
 cp 1_status.conf /etc/httpd/conf.d/
 
-user=`cat /opt/xovis3/xovis_remote_manager.properties | grep 'webgui.user' | cut -f2 -d'=' | tr -d [:space:]`
-passwd=`cat /opt/xovis3/xovis_remote_manager.properties | grep 'webgui.passwd' | cut -f2 -d'=' | tr -d [:space:]`
+mkdir -p /opt/xovis/status/
+cp xovis_ibex.properties /opt/xovis/status/
+
+user=`cat /opt/xovis/status/xovis_ibex.properties | grep 'webgui.user' | cut -f2 -d'=' | tr -d [:space:]`
+passwd=`cat /opt/xovis/status/xovis_ibex.properties | grep 'webgui.passwd' | cut -f2 -d'=' | tr -d [:space:]`
 /usr/bin/htpasswd -cdb /var/www/status/.htpasswd ${user} ${passwd}
 
 # Restart the Apache Web Server
